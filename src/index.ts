c00f45ae687d4cdc065fd4f928dbe632a6f47437
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-10-02 01:31:21 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.set('PORT', process.env.PORT || 3000);
+app.use(bodyParser.json());
+
+const NODE_ENV = process.env.NODE_ENV;
+if (NODE_ENV === 'dev') {
+    app.use(cors());
+}
+app.use('/', (req,res)=>{
+    res.send('hello world');
+});
+
+app.listen(app.get('PORT'), () => {
+    console.log('Server listening on port ', app.get('PORT'));
+});
