607400713f02e3fd19f477a4d4fc23197971d02e
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-03 00:52:44 +0530

    fix: Clean dependencies, fix repo lint in package.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,14 +10,16 @@ const app = express();
 app.set('PORT', process.env.PORT || 3000);
 app.use(bodyParser.json());
 
-const NODE_ENV = process.env.NODE_ENV;
+const { NODE_ENV } = process.env;
+
 if (NODE_ENV === 'dev') {
     app.use(cors());
 }
-app.use('/', (req,res)=>{
+
+app.use('/', (_req, res) => {
     res.send('hello world');
 });
 
 app.listen(app.get('PORT'), () => {
-    console.log('Server listening on port ', app.get('PORT'));
+    console.log('Server listening on port', app.get('PORT'));
 });
